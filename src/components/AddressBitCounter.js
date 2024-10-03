5a280855fa4856bf44172262f690f17cf12855f7
Urbil Melin <urbiltxo@gmail.com>
2024-10-03 13:05:51 +0200

    AddressBitCounterFuncionando correctamente

--- src/components/AddressBitCounter.js
+++ src/components/AddressBitCounter.js
@@ -1,33 +1,5 @@
 import React from 'react';
-function getWordBits(wordSize, blockSize){
-return Math.log2(blockSize/wordSize);
-}
-function getByteBits(wordSize){
-  if(wordSize!=null){
-    return Math.log2(wordSize);
-  }
-  return Math.log2(0);
 
-  
-}
-function getSetBits(way){
- return Math.log2(8/way);
-}
-function getAddressBits(mmSize){
-  return Math.log2(mmSize)
-}
-function getTagBits(cacheType,mmSize, setBits,wordBits,byteBits){
-  if(cacheType!=1){//Checking the cache type
-    return getAddressBits(mmSize)-setBits-wordBits-byteBits;
-
-  }
-  else{
-    return getAddressBits(mmSize)-wordBits-byteBits;
-
-  }
-
-  
-}
 function getSetOrBlock(formConfig){
   if (formConfig.cacheType == 1){
     return "Block";
@@ -37,7 +9,11 @@ function getSetOrBlock(formConfig){
   }
 }
 function AddressBitCounter({formConfig}) {
-
+    let addressBits = Math.log2(formConfig.mainMemorySize);
+    let byte =  Math.log2(formConfig.wordSize);
+    let word = Math.log2(formConfig.blockSize/formConfig.wordSize);
+    let block = Math.log2(8 / formConfig.cacheType)
+    let tag = addressBits - byte - word -block;
 
     return (
         <>
@@ -62,10 +38,10 @@ function AddressBitCounter({formConfig}) {
         </thead>
         <tbody>
           <tr>
-            <th scope="row" id="tag">{getTagBits(formConfig.cacheType,formConfig.mainMemorySize,getSetBits(formConfig.cacheType),getWordBits(formConfig.wordSize,formConfig.blockSize),getByteBits(formConfig.wordSize))}</th>
-            <td id="set">{getSetBits(formConfig.cacheType)}</td>
-            <td id="word">{getWordBits(formConfig.wordSize,formConfig.blockSize)}</td>
-            <td id="">{getByteBits(formConfig.wordSize)}</td>
+            <th>{tag}</th>
+            <td id="set">{block}</td>
+            <td id="word">{word}</td>
+            <td id="">{byte}</td>
           </tr>
          </tbody>
          
